bc658d3dab7d25de33329eacb720769799d2d2a8
dimitarmileski <dimit@r.com>
2019-07-26 14:53:26 +0200

    Claim model prop added

--- ClaimReq/Models/Claim.cs
+++ ClaimReq/Models/Claim.cs
@@ -6,6 +6,24 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Type => "Profile";
+
+        //Video Info
+        public string VideoTitle { get; set; }
+        public string VideoUrl { get; set; }
+        public string VideoId { get; set; }
+        public string VideoCategory { get; set; }
+        public string VideoDescription { get; set; }
+        public string VideoTags { get; set; }
+        public string VideoDatePublished { get; set; }
+        public string VideoChannelTitle { get; set; }
+        public string altUrlUpload { get; set; }
+
+
+        //Claim info
+        public string Description { get; set; }
+        public string Date { get; set; }
+        public string Status { get; set; }
+
     }
     // end::class[]
 }

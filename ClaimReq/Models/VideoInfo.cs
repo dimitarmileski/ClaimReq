eef396ef9aa9404bd95286d8d3ef094af3ae198a
dimitarmileski <dimit@r.com>
2019-08-14 12:29:52 +0200

    Document restructuring

---
+++ ClaimReq/Models/VideoInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClaimReq.Models
+{
+    public class VideoInfo
+    {
+        //Video Info
+
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+
+        public string Id { get; set; }
+
+        public string Category { get; set; }
+
+        public string Description { get; set; }
+
+        public string Tags { get; set; }
+
+        public string DatePublished { get; set; }
+
+        public string ChannelTitle { get; set; }
+    }
+}
